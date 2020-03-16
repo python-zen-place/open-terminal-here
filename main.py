2a4c28516fb4c86b33f391c0c8b0a20edc521b14
Weilet <weilet_sun@outlook.com>
2020-03-16 14:51:17 +0800

    simplify options and support deletion

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# -*- coding : utf-8 -*-
 import ctypes
 import getpass
 import glob
@@ -25,27 +26,8 @@ def get_terminal_path():
 	else:
 		return terminal_path[0].replace('\\', '\\\\')
 
-def choose_function():
-	menu_content = '''请选择功能：
-	1.为空白区域添加右键菜单：在此打开Windows Terminal
-	2.为目录添加右键菜单：在此目录打开Windows Terminal
-	3.同时添加1和2中的右键菜单
-	4.我不小心按到的，我要退出
-	'''
-	print(menu_content)
-	while True:
-		choice = input('请输入功能选项\n')
-		if choice in ('1', '2', '3', '4'):
-			return int(choice)
-		else:
-			print('输入有误')
-
-def open_wt_for_empty_area():
-	text = input('为右键菜单：在此打开Windows Terminal更改文字'
-		'（默认为在此打开Windows Terminal）\n')
-	if text == '':
-		text = '在此打开Windows Terminal'
-	reg = f'''Windows Registry Editor Version 5.00
+def render_reg_for_empty_area(text):
+	reg = f'''
 	[HKEY_CLASSES_ROOT\Directory\Background\shell\wt]
 	@="{text}"
 	"Icon"="{icon_path},0"
@@ -53,18 +35,10 @@ def open_wt_for_empty_area():
 	[HKEY_CLASSES_ROOT\Directory\Background\shell\wt\command]
 	@="{terminal_path} -d ."
 	'''
-	with open('open_wt_for_empty_area.reg', 'w') as f:
-		f.write(reg)
-	os.system('regedit /s open_wt_for_empty_area.reg')
-	os.system('del open_wt_for_empty_area.reg')
+	return reg
 
-def open_wt_for_selected_dir():
-	text = input('为右键菜单：在此目录打开Windows Terminal更改文字'
-		'（默认为在此目录打开Windows Terminal）\n')
-	if text == '':
-		text = '在此目录打开Windows Terminal'
-	reg = f'''Windows Registry Editor Version 5.00
-
+def render_reg_for_selected_dir(text):
+	reg = f'''
 	[HKEY_CLASSES_ROOT\Directory\shell\wt]
 	@="{text}"
 	"Icon"="{icon_path},0"
@@ -73,26 +47,56 @@ def open_wt_for_selected_dir():
 	@="{terminal_path} -d %V"
 
 	'''
-	with open('open_wt_for_selected_dir.reg', 'w') as f:
-		f.write(reg)
-	os.system('regedit /s open_wt_for_selected_dir.reg')
-	os.system('del open_wt_for_selected_dir.reg')
+	return reg
+
+def creat_right_click_menu():
+	text  = input('请输入右键菜单文字（默认为：在此打开Windows Terminal）\n')
+	if text == '':
+		text = '在此打开Windows Terminal'
+	with open('wt.reg', 'w') as f:
+		f.writelines('Windows Registry Editor Version 5.00')
+		f.write(render_reg_for_empty_area(text))
+		f.write(render_reg_for_selected_dir(text))
+	os.system('regedit /s wt.reg')
+	os.system('del wt.reg')
+
+def del_right_click_menu():
+	with open('del-wt.reg', 'w') as f:
+		f.write('Windows Registry Editor Version 5.00\n')
+		f.write('[-HKEY_CLASSES_ROOT\Directory\Background\shell\wt]\n')
+		f.write('[-HKEY_CLASSES_ROOT\Directory\shell\wt]\n')
+	os.system('regedit /s del-wt.reg')
+	os.system('del del-wt.reg')
+
+def choose_function():
+	menu_content = '''Windows Terminal 右键菜单添加脚本
+	请选择功能：
+	1.添加菜单
+	2.删除菜单
+	3.退出
+	'''
+	print(menu_content)
+	while True:
+		choice = input('请输入功能选项\n')
+		if choice in ('1', '2', '3'):
+			return int(choice)
+		else:
+			print('输入有误')
+
 
 if __name__ == '__main__':
-	if is_admin() == False:
-		print('请以管理员身份运行脚本')
-	else:
+	if is_admin():
 		icon_path = get_icon_path()
 		terminal_path = get_terminal_path()
 		choice = choose_function()
 		if choice == 1:
-			open_wt_for_empty_area()
+			creat_right_click_menu()
 		elif choice == 2:
-			open_wt_for_selected_dir()
+			del_right_click_menu()
 		elif choice == 3:
-			open_wt_for_empty_area()
-			open_wt_for_selected_dir()
-		elif choice == 4:
 			sys.exit()
 		print('完成')
 		os.system('pause')
+	else:
+		print('请以管理员身份运行脚本')
+		
